cec738c5d7c0e6e141edd8f049fb88b416052daa
송우석 <petersohng@gmail.com>
2024-02-02 10:50:26 +0900

    html, css 파일

---
+++ system.js
@@ -0,0 +1,13 @@
+
+// 현재 시간을 표시
+function updateTime() {
+    const currentTimeElement = document.getElementById('current-time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    currentTimeElement.textContent = formattedTime;
+}
+
+setInterval(updateTime, 1000);
