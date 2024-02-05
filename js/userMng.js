69a4fdfefcbe78e694927318c5f504bc014544ea
박종화 <pjh5745239@gmail.com>
2024-02-05 12:29:33 +0900

    no message

--- js/userMng.js
+++ js/userMng.js
@@ -1,13 +1,9 @@
-// window.onload = function(){
-// }
 window.addEventListener("load", function () {
     bind();
 })
 
 function bind() {
     document.querySelector("#select_all1").addEventListener("click", function (event) {
-        // 클릭된 요소가
-        // check 상태라면
         let list_check1 = document.querySelectorAll(".chk1")
 
         if (event.target.checked) {
@@ -23,8 +19,6 @@ function bind() {
         }
     });
     document.querySelector("#select_all2").addEventListener("click", function (event) {
-        // 클릭된 요소가
-        // check 상태라면
         let list_check2 = document.querySelectorAll(".chk2")
 
         if (event.target.checked) {
@@ -40,8 +34,6 @@ function bind() {
         }
     });
     document.querySelector("#select_all3").addEventListener("click", function (event) {
-        // 클릭된 요소가
-        // check 상태라면
         let list_check3 = document.querySelectorAll(".chk3")
 
         if (event.target.checked) {
@@ -57,8 +49,6 @@ function bind() {
         }
     });
     document.querySelector("#select_all4").addEventListener("click", function (event) {
-        // 클릭된 요소가
-        // check 상태라면
         let list_check4 = document.querySelectorAll(".chk4")
 
         if (event.target.checked) {
@@ -74,8 +64,6 @@ function bind() {
         }
     });
     document.querySelector("#select_all5").addEventListener("click", function (event) {
-        // 클릭된 요소가
-        // check 상태라면
         let list_check5 = document.querySelectorAll(".chk5")
 
         if (event.target.checked) {
