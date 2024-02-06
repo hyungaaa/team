b74b6f14ecc4ac904e9d2defb7579f89784e44f2
임가현 <dlarkgus09@naver.com>
2024-02-06 15:25:37 +0900

    item, capacity 수정본 0206

--- js/item.js
+++ js/item.js
@@ -13,34 +13,38 @@ function saveShow() {
 // 검색결과 나오게 하는 거 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // 검색 버튼 요소와 검색어 입력란 요소를 가져옵니다.
+    // 검색버튼, 입력 요소 가져오기
     var searchButton = document.querySelector(".main_btn_mng");
     var searchInput = document.querySelector(".main_search_txt_mng");
- 
-    // 검색 함수를 정의합니다.
+
+
     function performSearch() {
-        // 검색어를 가져옵니다.
         var searchKeyword = searchInput.value.toLowerCase();
-        // 테이블의 각 행을 가져옵니다.
         var rows = document.querySelectorAll("#productBoard table tr");
-        // 각 행에 대해 검색어와 일치하는 부분이 있는지 확인하고 보이거나 숨깁니다.
-        rows.forEach(function (row) {
+        rows.forEach(function (row, index) {
             var showRow = false;
-            // 각 행의 셀에 대해 검색어와 일치하는 부분이 있는지 확인합니다.
+
+            if (index === 0) {
+                showRow =  true;
+            }
+            else {
+            // 검색어 일치하는지 확인
             row.querySelectorAll("td").forEach(function (cell) {
                 if (cell.textContent.toLowerCase().includes(searchKeyword)) {
                     showRow = true;
                 }
             });
+        }
 
-            // 검색어와 일치하는 부분이 없으면 해당 행을 숨깁니다.
+            // 일치 하는 부분 없으면 행 숨김
             if (!showRow) {
                 row.style.display = "none";
             } else {
-                row.style.display = ""; // 일치하는 부분이 있으면 보이게 처리
+                row.style.display = ""; // 일치하는 부분 표시
             }
         });
-    }
+    };
+
 
     // 검색 버튼 누르면 검색 가능
     searchButton.addEventListener("click", performSearch);
@@ -52,3 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+
