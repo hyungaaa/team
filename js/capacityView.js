2ef8a4eabd40425b69d6893730ac9bd9f93df599
임가현 <dlarkgus09@naver.com>
2024-02-08 14:00:45 +0900

    주석 수정

--- js/capacityView.js
+++ js/capacityView.js
@@ -126,25 +126,20 @@ document.addEventListener("DOMContentLoaded", function () {
     var searchButton = document.querySelector(".main_btn_cap");
     var searchInput = document.querySelector(".main_search_txt_cap");
 
-    // 검색 함수를 정의합니다.
     function performSearch() {
         // 검색어를 가져옵니다.
         var searchKeyword = searchInput.value.toLowerCase();
-
         // 테이블의 각 행을 가져옵니다.
         var rows = document.querySelectorAll(".cap_view_table2 table tr");
-
         // 각 행에 대해 검색어와 일치하는 부분이 있는지 확인하고 보이거나 숨깁니다.
         rows.forEach(function (row) {
             var showRow = false;
-
             // 각 행의 셀에 대해 검색어와 일치하는 부분이 있는지 확인합니다.
             row.querySelectorAll("td").forEach(function (cell) {
                 if (cell.textContent.toLowerCase().includes(searchKeyword)) {
                     showRow = true;
                 }
             });
-
             // 검색어와 일치하는 부분이 없으면 해당 행을 숨깁니다.
             if (!showRow) {
                 row.style.display = "none";
@@ -154,9 +149,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+
     // 검색 버튼 누르면 검색 가능
     searchButton.addEventListener("click", performSearch);
-
     // 엔터 누르면 검색 가능
     searchInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
