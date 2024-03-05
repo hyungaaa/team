95256a6ca58162337310644f6cfdf32c263046c1
박종화 <pjh5745239@gmail.com>
2024-03-05 15:19:53 +0900

    사용자 권한관리

--- jal_project/src/main/java/Mng/UserMngServlet.java
+++ jal_project/src/main/java/Mng/UserMngServlet.java
@@ -1,8 +1,8 @@
 package Mng;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,18 +18,22 @@ public class UserMngServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// DAO를 통해 모든 테이블에서 데이터 조회
 		UserMngDAO userMngDAO = new UserMngDAO();
-		List<UserMngDTO> userList = null;
+		UserMngDAO2 userMngDAO2 = new UserMngDAO2();
+		List<UserMngDTO> resultList = null;
+		List<UserMngDTO2> resultList2 = null;
 		try {
-			userList = userMngDAO.selectAllTables();
-		} catch (Exception e) {
+			resultList = userMngDAO.selectAllTables();
+			resultList2 = userMngDAO2.selectAllTables2();
+		} catch (SQLException e) {
 			e.printStackTrace();
-			// 예외 처리
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database Error");
+			// 데이터베이스 오류 처리
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database Error: " + e.getMessage());
 			return;
 		}
 
 		// 결과를 request에 저장
-		request.setAttribute("userList", userList);
+		request.setAttribute("userList", resultList);
+		request.setAttribute("userList2", resultList2);
 
 		// view로 포워딩
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/userMng.jsp");
